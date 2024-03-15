d1f94d9e8449332e2514ef627ef4963e61219d20
Lemon <me@lemonbro.ru>
2024-03-15 14:32:00 +0500

    initial commit

---
+++ Money.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REA_OOP
+{
+    internal class Money
+    {
+        private int _rubles;
+        private int _kopeks;
+        private static int _counter;
+
+        public Money(int rubles, int kopeks)
+        {
+            this.Rubles = rubles;
+            this.Kopeks = kopeks;
+            _counter++;
+        }
+
+        public Money(int rubles)
+        {
+            new Money(rubles, 0);
+        }
+        public Money()
+        {
+            new Money(0, 0);
+        }
+
+        public static int Counter => _counter;
+
+        public int Rubles
+        {
+            get => _rubles;
+            set
+            {
+                if (value >= 0)
+                {
+                    _rubles = value;
+                }
+            }
+        }
+
+        public int Kopeks
+        {
+            get => _kopeks;
+            set
+            {
+                if ((value >= 0) && (value <= 99))
+                {
+                    _kopeks = value;
+                } else
+                {
+                    _rubles += value / 100;
+                    _kopeks = value % 100;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Rubles: " + Rubles + ", Kopeks: " + Kopeks;
+        }
+
+        public static Money operator +(Money a, int b)
+        {
+            return new Money(a.Rubles, a.Kopeks + b);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using REA_OOP;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Money a1 = new Money();
+Money a2 = new Money(123, 206);
+Money a3 = new Money(123, 123);
+Money a4 = new Money(999);
+Console.WriteLine(Money.Counter);
+Console.WriteLine(a2.ToString());
+a2 = a2 + (-10);
+Console.WriteLine(a2.ToString());
+a1.Kopeks = 99;
+a1.Rubles = 1000;
+Console.WriteLine(a1.ToString());
+Console.WriteLine(a3.ToString());
+Console.WriteLine(a4.ToString());
